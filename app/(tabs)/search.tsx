e50a530b68f46cc093dfc5395d6cb9df95294990
aduong3 <mythalink@gmail.com>
2025-04-18 15:10:50 -0700

    Install appwrite packages, able to fetch data with search query
    updateSearchCount has some bugs. If searching from one movie after another without reseting the search query, it will use the previous' movie data instead of the current search query.

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -7,6 +7,7 @@ import useFetch from "@/services/useFetch";
 import { fetchMovies } from "@/services/api";
 import { icons } from "@/constants/icons";
 import SearchBar from "@/components/SearchBar";
+import { updateSearchCount } from "@/services/appwrite";
 
 const search = () => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -23,12 +24,16 @@ const search = () => {
     const timeoutId = setTimeout(async () => {
       if (searchQuery.trim()) {
         await loadMovies();
+        if (movies?.length > 0 && movies?.[0])
+          await updateSearchCount(searchQuery, movies[0]);
       } else {
         reset();
       }
     }, 500);
 
-    return () => clearTimeout(timeoutId);
+    return () => {
+      clearTimeout(timeoutId);
+    };
   }, [searchQuery]);
 
   return (
